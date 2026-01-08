a6ca961bf5b8450c4286ac770f7b1d5f052e12b7
abhijeet7py <abhijeetjathar709@gmail.com>
2026-01-08 22:05:01 +0530

    New changes

--- Interview Ques/2_palindrom_checker.py
+++ Interview Ques/2_palindrom_checker.py
@@ -1,22 +1,22 @@
-# str = input("Enter a string: ")
+str = input("Enter a string: ")
 
-# # 1. using slicing method
-#
-# if str == str[::-1]: # Reverse using slicing and compare with original
-#     print(f"{str} is a palindrome.")
-#
-# else:
-#     print(f"{str} is not a palindrome.")
+# 1. using slicing method
+
+if str == str[::-1]: # Reverse using slicing and compare with original
+    print(f"{str} is a palindrome.")
+
+else:
+    print(f"{str} is not a palindrome.")
 
 # 2. Using 2 pointer method (Without a built-in method)
 
-s = input("Enter a string: ").lower().split()
+s = input("Enter a string: ").lower().split() # split the string into list of words
 
-left = 0
-right = len(s) - 1
+left = 0 # points to the first word in the list
+right = len(s) - 1 #points to the last word in the list
 
-while left < right:
-    if s[left] != s[right]:
+while left < right: # Loop continues until two pointers are not crossed
+    if s[left] != s[right]: # compares words from beginning to end
         print("Not a palindrome.")
         break
     left += 1
